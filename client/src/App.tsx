d23dae734700c2aa76bfb1ce06f6d73a06690ea4
Michal Blach <michal.blach@opuscapita.com>
2024-12-03 15:38:47 +0100

    fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './style/App.scss';
 import AstronautControlPanel from './components/AstronautControlPanel';
-import Navigation from './components/Navigation';
 import SolarSystemBackground from './components/SolarSystemBackground';
 
 function App() {
