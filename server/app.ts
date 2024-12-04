f2148faabd567d0a04650788a48af2a5151e6a97
Michal Blach <michal.blach@opuscapita.com>
2024-12-04 12:12:49 +0100

    rate limit removal

--- server/app.ts
+++ server/app.ts
@@ -6,24 +6,13 @@ import swaggerSpecs from './swaggerConfig';
 import config from './src/config/config';
 import cors from 'cors';
 import astronautsRoute from './src/routes/astronautsRoutes';
-import rateLimit from 'express-rate-limit';
 import helmet from 'helmet';
 
 const app = express();
 const port = config.app.port || 3001;
 
-const apiLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000,
-    max: 100,
-    message: {
-        error: 'Too many requests, please try again later.',
-    },
-    standardHeaders: true,
-    legacyHeaders: false,
-});
 
 app.use(helmet());
-app.use('/api', apiLimiter);
 
 app.use(cors({
     origin: config.app.corsOrigin,
